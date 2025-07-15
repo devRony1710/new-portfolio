7db75b8bd4176adff8fc59df79a34673982f47b1
Rony Antolinez <rony1710.dev@gmail.com>
2025-07-14 22:59:09 -0500

    feat: add alias config for resolve paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Test } from '@/components/Test';
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        ss
-      </div>
-      <h1 className="text-red-500">Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Test />
     </>
   );
 }
