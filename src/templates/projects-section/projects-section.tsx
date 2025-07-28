63a6b68eee5d0fc2a6c3cef9e2c6d712c46915f7
Rony Antolinez <rony1710.dev@gmail.com>
2025-07-28 12:59:49 -0500

    feat: add info from github api

--- src/templates/projects-section/projects-section.tsx
+++ src/templates/projects-section/projects-section.tsx
@@ -1,30 +1,29 @@
-import { Projects } from '@/data/projects';
 import { AppContext } from '@/context/app-context';
 import { useContext } from 'react';
 
 export const ProjectsSection = () => {
-  const { userInfo } = useContext(AppContext);
-  console.log(userInfo);
+  const { reposInfo } = useContext(AppContext);
+  console.log(reposInfo);
 
   return (
     <section className="w-full h-auto max-w-[850px] mx-auto gap-[1rem]">
       <section className="w-full grid grid-cols-2 gap-[1rem]">
-        {Projects.map((project, index) => (
+        {reposInfo?.map((repo, index) => (
           <div key={index} className="w-full max-w-[380px] h-auto max-h-[398px] p-[1rem] hover:bg-zinc-100/10 rounded-[1rem]">
-            <div className="w-full h-[200px] bg-radial-[at_50%_75%] from-sky-200 via-bgPrimary to-bgPrimary to-90% rounded-lg relative">
+            {/* <div className="w-full h-[200px] bg-radial-[at_50%_75%] from-sky-200 via-bgPrimary to-bgPrimary to-90% rounded-lg relative">
               <img
                 src={project.projectImage}
                 alt={project.projectName}
                 className="w-[95%] hover:scale-[1.4] transition-all duration-300 z-[10] ease-in-out rotate-[3deg] translate-y-[5%] translate-x-[5%] scale-[1.1] absolute mx-auto rounded-lg h-[180px] object-fill"
               />
-            </div>
+            </div> */}
 
             <div className="font-inter mt-6 text-white w-full flex flex-col h-auto gap-[12px]">
-              <span className="text-[22px] font-bold">{project.projectName}</span>
-              <span className="text-[18px] text-zinc-400">{project.projectDescription}</span>
+              <span className="text-[22px] font-bold">{repo?.name}</span>
+              <span className="text-[18px] text-zinc-400">{repo?.description}</span>
             </div>
 
-            <a target="_blank" href={project.projectLink} rel="noreferrer">
+            <a target="_blank" href={repo?.homepage} rel="noreferrer">
               Link
             </a>
           </div>
